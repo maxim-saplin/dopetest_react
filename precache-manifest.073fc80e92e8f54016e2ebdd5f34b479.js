1fee1c405166483098a87e1dbe923d3b30da7417
sdfsfd <sdfd@sdfsd.ff>
2020-06-05 23:04:33 +0300

    Updates

---
+++ precache-manifest.073fc80e92e8f54016e2ebdd5f34b479.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2b9b48148f73caeec6169a65a5f09327",
+    "url": "/dopetest_react/index.html"
+  },
+  {
+    "revision": "9a6bc21b57ec47e3e3ed",
+    "url": "/dopetest_react/static/css/main.c1e4e9ef.chunk.css"
+  },
+  {
+    "revision": "24a03b08fd232a22da15",
+    "url": "/dopetest_react/static/js/2.4ae32c79.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/dopetest_react/static/js/2.4ae32c79.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9a6bc21b57ec47e3e3ed",
+    "url": "/dopetest_react/static/js/main.93532f78.chunk.js"
+  },
+  {
+    "revision": "644a9b609a7defdf500c",
+    "url": "/dopetest_react/static/js/runtime-main.f3c8c91a.js"
+  },
+  {
+    "revision": "5632af57e4d486d7597865250c6b1e77",
+    "url": "/dopetest_react/static/media/IBMPlexMono-Regular.5632af57.woff"
+  }
+]);
